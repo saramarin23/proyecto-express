871b8cdc00e15444d220e8f0905f0beba35663dd
saramarin23 <saramarin.econ@gmail.com>
2020-01-26 19:34:28 +0100

    Prepare server to work, install nodemon and prettier and handle errors

--- app.js
+++ app.js
@@ -9,17 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// http:localhost:3000/
 app.use('/', indexRouter);
+
+// http:localhost:3000/users
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -34,7 +33,10 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  const errorMessages = {
+    404: 'Page not found',
+  };
+  res.status(err.status || 500).json(errorMessages[err.status] || err.message);
   res.render('error');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// http://localhost:3000/ping
+router.get("/ping", function(req, res, next) {
+  res.status(200).json("Server working properly!");
 });
 
 module.exports = router;
